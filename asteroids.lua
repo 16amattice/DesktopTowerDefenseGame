794d8c91e00ea8c8b689ad1d947b98d4bdf648be
Anthony Mattice <AMattice@desert-aire.com>
2023-12-29 11:02:28 -0600

    Make asteroids spin, and give asteroids a polygon shape.

--- asteroids.lua
+++ asteroids.lua
@@ -1,13 +1,29 @@
 local Asteroids = {}
 _G.asteroids = {}
 
+
 function Asteroids.spawn()
     local ax, ay
     repeat
         ax = love.math.random(0, _G.window_width)
         ay = love.math.random(0, _G.window_height)
     until math.sqrt((_G.x + _G.square_size / 2 - ax)^2 + (_G.y + _G.square_size / 2 - ay)^2) >= _G.devSettings.minDistance.value
-    table.insert(_G.asteroids, {x = ax, y = ay, speed = _G.devSettings.enemySpeed.value})
+
+    local vertices = {}
+    local numVertices = love.math.random(5, 8)
+    local angleStep = (2 * math.pi) / numVertices
+    local asteroidSize = love.math.random(15, 30)
+
+    for i = 0, numVertices - 1 do
+        local angle = i * angleStep
+        local radius = asteroidSize + love.math.random(-5, 5)
+        local x = ax + radius * math.cos(angle)
+        local y = ay + radius * math.sin(angle)
+        table.insert(vertices, x)
+        table.insert(vertices, y)
+    end
+    local rotationSpeed = love.math.random(-0.2, 0.2)
+    table.insert(_G.asteroids, {x = ax, y = ay, speed = _G.devSettings.enemySpeed.value, rotationSpeed = rotationSpeed, vertices = vertices})
 end
 
 function Asteroids.update(dt)
@@ -16,21 +32,33 @@ function Asteroids.update(dt)
         local dirX = _G.x + _G.square_size / 2 - asteroid.x
         local dirY = _G.y + _G.square_size / 2 - asteroid.y
         local length = math.sqrt(dirX^2 + dirY^2)
+        local rotation = asteroid.rotationSpeed * dt
         dirX = dirX / length
         dirY = dirY / length
 
-        asteroid.x = asteroid.x + dirX * asteroid.speed * dt
-        asteroid.y = asteroid.y + dirY * asteroid.speed * dt
+        local dx = dirX * asteroid.speed * dt
+        local dy = dirY * asteroid.speed * dt
 
+        asteroid.x = asteroid.x + dx
+        asteroid.y = asteroid.y + dy
+
+        for j = 1, #asteroid.vertices, 2 do
+            local vx = asteroid.vertices[j] - asteroid.x + dx
+            local vy = asteroid.vertices[j + 1] - asteroid.y + dy
+            asteroid.vertices[j] = asteroid.x + vx * math.cos(rotation) - vy * math.sin(rotation)
+            asteroid.vertices[j + 1] = asteroid.y + vx * math.sin(rotation) + vy * math.cos(rotation)
+        end
+        
         if _G.utils.checkCollision(asteroid.x, asteroid.y, 20, _G.x, _G.y, _G.square_size) then
             table.remove(_G.asteroids, i)
         end
     end
 end
 
+
 function Asteroids.draw()
-    for i, asteroid in ipairs(_G.asteroids) do
-        love.graphics.circle("line", asteroid.x, asteroid.y, 20)
+    for _, asteroid in ipairs(_G.asteroids) do
+        love.graphics.polygon("line", asteroid.vertices)
     end
 end
 
